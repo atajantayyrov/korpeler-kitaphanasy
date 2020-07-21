83f6186be496d996a49ef54638bc431b6774bcab
atajan <atadzantajyrov020@gmail.com>
2020-07-21 16:57:11 -0700

    header-kindsbook

---
+++ web-parts/header.php
@@ -0,0 +1,40 @@
+<header>
+        <div class="kk-header">
+                <div class="kk-container">
+                    <div class="kk-logo">
+                        <div class="kk-logo__image">
+                            <img src="img/logo.png" alt="img1" />
+                        </div>
+                        <div class="kk-logo__text">
+                            <a href="#!">
+                                <span>Körpeler</span>
+                                <span>Kitaphanasy</span>
+                            </a>
+                        </div>
+                    </div>
+                    <nav class="kk-nav" id="kkNav">
+                        <ul class="kk-nav__list">
+                            <li class="kk-nav__item">
+                                <a href="#!" class="kk-nav__link">Kitalar</a>
+                            </li>
+                            <li class="kk-nav__item">
+                                <a href="#!" class="kk-nav__link">Oýunjaklar</a>
+                            </li>
+                            <li class="kk-nav__item">
+                                <a href="#!" class="kk-nav__link">Döredijilik</a>
+                            </li>
+                            <li class="kk-nav__item">
+                                <a href="#!" class="kk-nav__link">Zehin özgertme</a>
+                            </li>
+                            <li class="kk-nav__item">
+                                <a href="#!" class=" kk-nav__button">
+                            
+                                <i class="fa fa-shopping-basket" aria-hidden="true"></i><span>26</span></a>
+                                <a href="#!" class=" kk-nav__button">
+                                <i class="fa fa-search" aria-hidden="true"></i></a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+        </div>
+</header>
---
+++ web-parts/kindsbook.php
@@ -0,0 +1,35 @@
+<section class="kk-kindsbook">
+        <div class="kk-container">
+          <div class="kk-kindsbook__wrapper">
+              <div class="kk-kindsbook__main">
+              <div class="kk-kindsbook__text1">
+              
+                      <p>Dünýa</p><h2><span>kitap</span></h2><p> bilen gyzykly</p>
+                </div>
+              <div class="kk-kindsbook__img">
+            <img src="img/kids.png" alt="img2" />
+                </div>
+              </div>
+              <div class="kk-kindsbook__kinds">
+              <div class="kk-kindsbook__items">
+              <div class="kk-kindsbook__text">
+                <p><span>Erteki</span></p></div></div>
+                <div class="kk-kindsbook__items">
+              <div class="kk-kindsbook__text">
+                <p><span>Erteki</span></p></div></div>
+                <div class="kk-kindsbook__items">
+              <div class="kk-kindsbook__text">
+                <p><span>Erteki</span></p></div></div>
+                <div class="kk-kindsbook__items">
+              <div class="kk-kindsbook__text">
+                <p><span>Erteki</span></p></div></div>
+                <div class="kk-kindsbook__items">
+              <div class="kk-kindsbook__text">
+                <p><span>Erteki</span></p></div></div>
+                <div class="kk-kindsbook__items">
+              <div class="kk-kindsbook__text">
+                <p><span>Erteki</span></p></div></div>
+              </div>
+          </div>
+        </div>
+</section>
